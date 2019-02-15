33023b2b467f0ee5e9ca69127929faaddb8dbd33
awhitbeck <whitbeck.andrew@gmail.com>
2019-02-14 23:20:56 -0600

    updating cuts to compare with Bill

--- src/cutFlow.cc
+++ src/cutFlow.cc
@@ -30,37 +30,37 @@ int main(int argc, char** argv){
     vector<TString> sampleNames;
 
     TString skimType;
-    skimType="root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/Run2ProductionV16/tree_signal/";
+    skimType=BASE_DIR+"tree_signal/";
     
     vector<TString> ZJetsFileNames;
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-100to200_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-200to400_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-400to600_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-600to800_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-800to1200_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-1200to2500_MC2017.root");
-    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-2500toInf_MC2017.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-100to200_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-200to400_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-400to600_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-600to800_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-800to1200_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-1200to2500_MC2016.root");
+    ZJetsFileNames.push_back("tree_ZJetsToNuNu_HT-2500toInf_MC2016.root");
     TChain* ZJets = new TChain("tree");
     for( int i = 0 ; i < ZJetsFileNames.size() ; i++ ){
         ZJets->Add(skimType+"/"+ZJetsFileNames[i]);
     }
-    //samples.push_back(new RA2bTree(ZJets));
-    //sampleNames.push_back("ZJets");
+    samples.push_back(new RA2bTree(ZJets));
+    sampleNames.push_back("ZJets");
 
-    skimType="root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/Run2ProductionV16/tree_GJet_CleanVars";
+    skimType=BASE_DIR+"tree_GJet_CleanVars";
 
     vector<TString> GJetsFileNames;
-    GJetsFileNames.push_back("tree_GJets_HT-100to200_MC2017.root");
-    GJetsFileNames.push_back("tree_GJets_HT-200to400_MC2017.root");
-    GJetsFileNames.push_back("tree_GJets_HT-400to600_MC2017.root");
-    GJetsFileNames.push_back("tree_GJets_HT-600toInf_MC2017.root");
+    GJetsFileNames.push_back("tree_GJets_DR-0p4_HT-100to200_MC2016.root");
+    GJetsFileNames.push_back("tree_GJets_DR-0p4_HT-200to400_MC2016.root");
+    GJetsFileNames.push_back("tree_GJets_DR-0p4_HT-400to600_MC2016.root");
+    GJetsFileNames.push_back("tree_GJets_DR-0p4_HT-600toInf_MC2016.root");
     
     TChain* GJets = new TChain("tree");
     for( int i = 0 ; i < GJetsFileNames.size() ; i++ ){
         GJets->Add(skimType+"/"+GJetsFileNames[i]);
     }
-    samples.push_back(new RA2bTree(GJets));
-    sampleNames.push_back("GJets");
+    //samples.push_back(new RA2bTree(GJets));
+    //sampleNames.push_back("GJets");
     
     typedef plot<RA2bTree> plot;
     typedef bool(*cutFunc)(RA2bTree*);
@@ -68,22 +68,30 @@ int main(int argc, char** argv){
     vector<TString> cutName;
     cutFlow.push_back(*cutFlow_none<RA2bTree>);
     cutName.push_back("none");
-    cutFlow.push_back(*cutFlow_onePhoton<RA2bTree>);
-    cutName.push_back("onePhoton");
-    cutFlow.push_back(*cutFlow_photonPt200<RA2bTree>);
-    cutName.push_back("photonPt200");
-    cutFlow.push_back(*cutFlow_minDR<RA2bTree>);
-    cutName.push_back("minDR");
+    cutFlow.push_back(*cutFlow_HT300<RA2bTree>);
+    cutName.push_back("HT300");
+    cutFlow.push_back(*cutFlow_MHT300<RA2bTree>);
+    cutName.push_back("MHT300");
+    cutFlow.push_back(*cutFlow_NJets2plus<RA2bTree>);
+    cutName.push_back("NJets2");
+    cutFlow.push_back(*cutFlow_leptonVeto<RA2bTree>);
+    cutName.push_back("LeptonVeto");
+    // cutFlow.push_back(*cutFlow_onePhoton<RA2bTree>);
+    // cutName.push_back("onePhoton");
+    // cutFlow.push_back(*cutFlow_photonPt200<RA2bTree>);
+    // cutName.push_back("photonPt200");
+    // cutFlow.push_back(*cutFlow_minDR<RA2bTree>);
+    // cutName.push_back("minDR");
+    cutFlow.push_back(*cutFlow_filters<RA2bTree>);
+    cutName.push_back("filters");
+    cutFlow.push_back(*cutFlow_btagsZero<RA2bTree>);
+    cutName.push_back("btagsZero");
     cutFlow.push_back(*cutFlow_HTbin1<RA2bTree>);
     cutName.push_back("HTbin1");
     cutFlow.push_back(*cutFlow_MHTbin1<RA2bTree>);
     cutName.push_back("MHTbin1");
     cutFlow.push_back(*cutFlow_nJetsTwo<RA2bTree>);
     cutName.push_back("nJetsTwo");
-    cutFlow.push_back(*cutFlow_btagsZero<RA2bTree>);
-    cutName.push_back("btagsZero");
-    cutFlow.push_back(*cutFlow_filters<RA2bTree>);
-    cutName.push_back("filters");
 
     //vector<vector<plot> > plots;
     vector<vector<plot> > plots;
@@ -115,9 +123,9 @@ int main(int argc, char** argv){
             for( int iCut = 0 ; iCut < cutFlow.size() ; iCut++ ){
                 if( ! cutFlow[iCut](ntuple) ) break;
                 for( int iPlot = 0 ; iPlot < plots[iCut].size() ; iPlot++ ){
-                    weight = ntuple->Weight*lumi*customPUweights(ntuple);
+		  //weight = ntuple->Weight*lumi;
                     if( sampleNames[iSample] == "GJets" ){
-                        weight*=/*photonTriggerWeight(ntuple)**/GJets0p4Weights(ntuple)*dRweights(ntuple);
+		      weight*=1;// photonTriggerWeight(ntuple)*GJets0p4Weights(ntuple)*dRweights(ntuple);
                         plots[iCut][iPlot].fill(ntuple,weight);
                     }else 
                         plots[iCut][iPlot].fill(ntuple,weight);
@@ -126,7 +134,7 @@ int main(int argc, char** argv){
         }
     }
 
-    TFile* outputFile = new TFile("cutFlow_synchTroy_MC2017.root","RECREATE");
+    TFile* outputFile = new TFile("cutFlow_synchTroy_MC2016.root","RECREATE");
 
     for( int iCut = 0 ; iCut < cutFlow.size() ; iCut++ ){ 
         for( int iPlot = 0 ; iPlot < plots[iCut].size() ; iPlot++ ){ 
